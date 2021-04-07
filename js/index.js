8ce4229821baa0490951c8b8f9c742099dbcb533
huangshihyu <fomo80265@gmail.com>
2021-04-07 13:18:56 +0800

    0407-2

--- js/index.js
+++ js/index.js
@@ -18,3 +18,25 @@ function show(){
           console.log(`a>3`);
       }
 }
+$(window).scroll(function () {
+  var scrollVal = $(this).scrollTop();
+  // console.log('yuyu', scrollVal);
+
+  if (scrollVal > ($(".s1").offset().top - 100)) {
+      // console.log('ff', $(".sa").offset().top);
+      // $('body,html').animate({scrollTop: $('.s1').offset().top}, 800);
+  }
+
+});
+$('.menu p:nth-child(1)').on('click',function(){
+  $('body,html').animate({scrollTop: $('.s1').offset().top}, 800);
+})
+$('.menu p:nth-child(2)').on('click',function(){
+  $('body,html').animate({scrollTop: $('.s2').offset().top+130}, 800);
+})
+$('.menu p:nth-child(3)').on('click',function(){
+  $('body,html').animate({scrollTop: $('.s3').offset().top}, 800);
+})
+$('.menu p:nth-child(4)').on('click',function(){
+  $('body,html').animate({scrollTop: $('.s4').offset().top+100}, 800);
+})
